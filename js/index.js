7ea08a8d123ef29e4e3911c1397062fa993467b0
Keith Ammon <repfigit@users.noreply.github.com>
2024-12-15 20:30:21 -0500

    made fixes for mobile

--- js/index.js
+++ js/index.js
@@ -14,22 +14,37 @@ function loadEmergingTechList() {
     fetch('data/tech_list.csv')
         .then(response => response.text())
         .then(data => {
+            // parse and shuffle tech list data
             const rows = data.split('\n');
             let result = rows.map(row => row.split('|'));
-
             result = shuffleArray(result);
+
+            let frag = document.createDocumentFragment();
+
+            let lead = document.createElement('span');
+            lead.innerHTML = '<strong>Emerging Technologies:</strong> |···';
+            frag.appendChild(lead);
             
             result.forEach(row => {
                 const span = document.createElement('span');
                 const link = document.createElement('a');
-                span.appendChild(link);
                 link.href = row[1];
                 link.textContent = row[0];
                 link.target = '_blank';
-                m.appendChild(document.createTextNode('< '));
-                m.appendChild(span);
-                m.appendChild(document.createTextNode(' >...'));
+                span.appendChild(link);
+
+                frag.appendChild(document.createTextNode('| '));
+                frag.appendChild(span);
+                frag.appendChild(document.createTextNode(' |···'));
             });
+
+            let pacman = document.createElement('img');
+            pacman.className="pacman";
+            pacman.src = "img/pac-man-fortnite.gif";
+
+            frag.appendChild(pacman);
+
+            m.append(frag);
         });
 }
 
